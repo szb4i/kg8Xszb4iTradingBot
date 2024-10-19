fb1cc301b6ac06645f3419e2a6e86d376662eabb
githubkg8 <kosztolanyi.gabor95@gmail.com>
2024-10-19 17:21:20 +0200

    websocket_test

--- exchange/exchange_websocket.py
+++ exchange/exchange_websocket.py
@@ -1,1 +1,8 @@
 # websocket for receiving current prices from exchange. on each new message, tick strategy
+from binance import Client, ThreadedWebsocketManager, ThreadedDepthCacheManager
+
+client=Client(credentials.getBinanceKey(), credentials.getBinanceSecretKey())
+twm = ThreadedWebsocketManager(api_key=credentials.getBinanceKey(), api_secret=credentials.getBinanceSecretKey())
+twm.start()
+
+twm.start_symbol_ticker_socket(symbol="BTCUSDT", callback=handle_socket_message)
