f6565647d0cbca2e7c4e7671c27fd45def9b265c
szb4i <szabolcs.nagy.hu@gmail.com>
2024-11-10 11:42:46 +0100

    define some constants in Strategy class. add bollinger_bands to __set_technical_indicators. separete __set_stop_loss_init and __set_stop_loss_live

--- strategy/strategy.py
+++ strategy/strategy.py
@@ -4,6 +4,10 @@ from technical_analysis.ema import get_ema
 from technical_analysis.bollinger_bands import get_bollinger_bands
 from constants import Candle
 
+CAPITAL_RISK = 0.03
+STOPLOSS_THRESHOLD_BOTTOM_RATIO = 0.05
+STOPLOSS_THRESHOLD_TOP_RATIO = 0.005
+
 class Strategy:
     def __init__(self, historical_ohlc) -> None:
         self.ohlc = historical_ohlc
@@ -11,7 +15,6 @@ class Strategy:
         self.is_in_long = False
         self.stop_loss = None
         self.quantity = None
-        self.capital_at_risk=0.03
         self.leverage = 6
 
     def on_exchange_ws_message(self, message):
@@ -27,10 +30,11 @@ class Strategy:
         self.ema_5 = get_ema(self.ohlc, 5)
         self.ema_21 = get_ema(self.ohlc, 21)
         self.ema_55 = get_ema(self.ohlc, 55)
+        self.bollinger_bands = get_bollinger_bands(self.ohlc)
 
     def __open_long_position(self):
         if self.ema_21[-2] < self.ema_55[-2] and self.ema_21[-1] > self.ema_55[-1]:
-            self.__set_stop_loss()
+            self.__set_stop_loss_init()
             self.__set_quantity()
             # TODO
             # 3. pozi megnyitasa
@@ -38,10 +42,10 @@ class Strategy:
             # 5. logolas, email kuldes
             # 6. backtest
 
-    def __set_stop_loss(self):
-        threshold_bottom = self.ohlc[-1, Candle.CLOSE] * (1 - 0.05)
-        threshold_top = self.ohlc[-1, Candle.CLOSE] * (1 - 0.005)
-        bollinger_bands_last_bottom_price = get_bollinger_bands(self.ohlc)[2,-1]
+    def __set_stop_loss_init(self):
+        threshold_bottom = self.ohlc[-1, Candle.CLOSE] * (1 - STOPLOSS_THRESHOLD_BOTTOM_RATIO)
+        threshold_top = self.ohlc[-1, Candle.CLOSE] * (1 - STOPLOSS_THRESHOLD_TOP_RATIO)
+        bollinger_bands_last_bottom_price = self.bollinger_bands[2,-1]
 
         # if in position, check if SL can be modified
         if self.stop_loss is not None:
@@ -55,9 +59,15 @@ class Strategy:
             else:
                 self.stop_loss = bollinger_bands_last_bottom_price
 
+    def __set_stop_loss_live(self):
+        # bottom bollinger eleinte, aztan ha elerjuk a virtual TP szintet, atvaltunk trailing stoplossra
+        bollinger_bands_last_bottom_price = self.bollinger_bands[2,-1]
+        if bollinger_bands_last_bottom_price > self.stop_loss:
+            self.stop_loss = bollinger_bands_last_bottom_price
+
     def __set_quantity(self):
         # a toke hany szazalekaval nyitunk poziciot, 0 es (100*leverage) % kozotti ertek
-        position_size_pct_of_capital = self.capital_at_risk / (1 - self.stop_loss / self.ohlc[-1, Candle.CLOSE])
+        position_size_pct_of_capital = CAPITAL_RISK / (1 - self.stop_loss / self.ohlc[-1, Candle.CLOSE])
         usdc_capital = ExchangeRest.futures_usdc_balance 
         # ezt nem tudom honnan kéne, elkezdtem az ExchangeRest classban egy új attribútumot (?), 
         # de nem vagyok benne biztos h igy kéne, pls gondold már át te is.
