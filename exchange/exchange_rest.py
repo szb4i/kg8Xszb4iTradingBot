c161be3611f2a5cbc79dc9911c8af07f51c55d78
szb4i <szabolcs.nagy.hu@gmail.com>
2024-11-12 14:31:29 +0100

    roty: __manage_long_position

--- exchange/exchange_rest.py
+++ exchange/exchange_rest.py
@@ -9,7 +9,6 @@ from binance.enums import SIDE_BUY, FUTURE_ORDER_TYPE_MARKET
 class ExchangeRest:
     def __init__(self) -> None:
         self.client=Client(getBinanceKey(), getBinanceSecretKey())
-        self.futures_usdc_balance = self.get_futures_usdc_balance()
 
     def get_historical_ohlc(self):
         historical_klines = self.client.get_historical_klines(symbol = SYM, interval = CANDLE_INTERVAL, start_str=HISTORICAL_DATA_RELATIVE_START_DATE)
@@ -20,5 +19,5 @@ class ExchangeRest:
         self.client.futures_create_order(symbol=SYM, side=SIDE_BUY, type=FUTURE_ORDER_TYPE_MARKET, quantity=quantity, isIsolated=True)
 
     def get_futures_usdc_balance(self):
-        self.futures_usdc_balance = float(next((item['availableBalance'] for item in self.client.futures_account_balance() if item['asset'] == 'USDC'), 0))
-        print(float(next((item['availableBalance'] for item in self.client.futures_account_balance() if item['asset'] == 'USDC'), 0)))
+        futures_usdc_balance = float(next((item['availableBalance'] for item in self.client.futures_account_balance() if item['asset'] == 'USDC'), 0))
+        return futures_usdc_balance
--- strategy/strategy.py
+++ strategy/strategy.py
@@ -3,6 +3,7 @@ import numpy as np
 from technical_analysis.ema import get_ema
 from technical_analysis.bollinger_bands import get_bollinger_bands
 from constants import Candle
+from exchange.exchange_rest import ExchangeRest
 
 CAPITAL_RISK = 0.03
 STOPLOSS_THRESHOLD_BOTTOM_RATIO = 0.05
@@ -12,16 +13,28 @@ class Strategy:
     def __init__(self, historical_ohlc) -> None:
         self.ohlc = historical_ohlc
         self.__set_technical_indicators()
-        self.is_in_long = False
-        self.stop_loss = None
-        self.quantity = None
+        self.exchange_rest = ExchangeRest()
+        self.balance = self.exchange_rest.get_futures_usdc_balance()
         self.leverage = 6
 
+    def __set_variables_to_default(self):
+        self.is_in_long = False
+        self.stop_loss = None
+        self.is_trailing_stop_loss = False
+        self.trailing_stop_loss_step_ratio = None
+        self.take_profit_virtual = None
+        self.quantity = None
+
     def on_exchange_ws_message(self, message):
-        if message['x']:
+        is_candle_closing = message['x']
+        if is_candle_closing:
             self.__set_ohlc(message)
             self.__set_technical_indicators()
-
+            if not self.is_in_long:
+                self.__open_long_position()
+        if self.is_in_long:
+            current_price = message['k']['c']
+            self.__manage_long_position(current_price, is_candle_closing)
 
     def __set_ohlc(self, message):
         np.append([message['k']['o'], message['k']['h'], message['k']['l'], message['k']['c'], message['k']['v']])
@@ -35,10 +48,12 @@ class Strategy:
     def __open_long_position(self):
         if self.ema_21[-2] < self.ema_55[-2] and self.ema_21[-1] > self.ema_55[-1]:
             self.__set_stop_loss_init()
+            self.__set_take_profit_virtual()
+            self.__set_trailing_stop_loss_step_ratio()
             self.__set_quantity()
+            self.exchange_rest.open_long(self.quantity)
+            self.is_in_long = True
             # TODO
-            # 3. pozi megnyitasa
-            # 4. nyitott pozicio managelese. on_message-ben ha poziban vagy, akkor minden message-re csekkolod, hogy le kell-e zarni. vagy trailing stop loss, ha jo iranyba megy
             # 5. logolas, email kuldes
             # 6. backtest
 
@@ -46,24 +61,18 @@ class Strategy:
         threshold_bottom = self.ohlc[-1, Candle.CLOSE] * (1 - STOPLOSS_THRESHOLD_BOTTOM_RATIO)
         threshold_top = self.ohlc[-1, Candle.CLOSE] * (1 - STOPLOSS_THRESHOLD_TOP_RATIO)
         bollinger_bands_last_bottom_price = self.bollinger_bands[2,-1]
-
-        # if in position, check if SL can be modified
-        if self.stop_loss is not None:
-            if bollinger_bands_last_bottom_price > self.stop_loss:
-                self.stop_loss = bollinger_bands_last_bottom_price
+        if bollinger_bands_last_bottom_price > threshold_top:
+            self.stop_loss = threshold_top
+        elif bollinger_bands_last_bottom_price < threshold_bottom:
+            self.stop_loss = threshold_bottom
         else:
-            if bollinger_bands_last_bottom_price > threshold_top:
-                self.stop_loss = threshold_top
-            elif bollinger_bands_last_bottom_price < threshold_bottom:
-                self.stop_loss = threshold_bottom
-            else:
-                self.stop_loss = bollinger_bands_last_bottom_price
-
-    def __set_stop_loss_live(self):
-        # bottom bollinger eleinte, aztan ha elerjuk a virtual TP szintet, atvaltunk trailing stoplossra
-        bollinger_bands_last_bottom_price = self.bollinger_bands[2,-1]
-        if bollinger_bands_last_bottom_price > self.stop_loss:
             self.stop_loss = bollinger_bands_last_bottom_price
+
+    def __set_take_profit_virtual(self):
+        self.take_profit_virtual = self.ohlc[-1, Candle.CLOSE] + (self.ohlc[-1, Candle.CLOSE] - self.stop_loss)
+
+    def __set_trailing_stop_loss_step_ratio(self):
+        self.trailing_stop_loss_step_ratio = (1 - (self.stop_loss / self.ohlc[-1, Candle.CLOSE])) / 4
 
     def __set_quantity(self):
         # a toke hany szazalekaval nyitunk poziciot, 0 es (100*leverage) % kozotti ertek
@@ -74,3 +83,28 @@ class Strategy:
         # vagy behivni a get_futures_usdc_balance fuggvenyt csak az kicsit lassithat a pozinyitásunkon
         position_size = position_size_pct_of_capital * usdc_capital
         return position_size
+    
+    def __manage_long_position(self, current_price, is_candle_closing):
+        self.__close_long_position()
+        if self.is_in_long:
+            self.__set_stop_loss_live(current_price, is_candle_closing)
+
+    def __close_long_position(self, current_price):
+        if current_price < self.stop_loss:
+            # TODO
+            # close_long fuggveny implementalasa az ExchangeRestben
+            self.__set_variables_to_default()
+
+    def __set_stop_loss_live(self, current_price, is_candle_closing):
+        # TODO
+        # ha valtozik a stop_loss, azt az exchange-en is kezelni kell
+        if current_price > self.take_profit_virtual:
+            self.stop_loss = self.take_profit_virtual * (1 - self.trailing_stop_loss_step_ratio)
+            self.take_profit_virtual = self.take_profit_virtual * (1 + self.trailing_stop_loss_step_ratio)
+            self.is_trailing_stop_loss = True
+        elif is_candle_closing and not self.is_trailing_stop_loss:
+            bollinger_bands_last_bottom_price = self.bollinger_bands[2,-1]
+            if bollinger_bands_last_bottom_price > self.stop_loss:
+                self.stop_loss = bollinger_bands_last_bottom_price
+
+
