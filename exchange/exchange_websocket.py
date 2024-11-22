bb97250eaf038f9769421dd2d2b78521491dc55c
szb4i <szabolcs.nagy.hu@gmail.com>
2024-11-22 16:55:20 +0100

    fix interval in exchange_websocket

--- exchange/exchange_websocket.py
+++ exchange/exchange_websocket.py
@@ -2,10 +2,11 @@
 from binance import ThreadedWebsocketManager
 from credentials import getBinanceKey, getBinanceSecretKey
 from typing import Callable
+from constants import CANDLE_INTERVAL
  
 class ExchangeWebsocket:
     def __init__(self, on_exchange_ws_message: Callable) -> None:
         twm = ThreadedWebsocketManager(api_key=getBinanceKey(), api_secret=getBinanceSecretKey())
         twm.start()
-        twm.start_kline_socket(symbol="BTCUSDT", callback=on_exchange_ws_message)
+        twm.start_kline_socket(symbol="BTCUSDT", callback=on_exchange_ws_message, interval=CANDLE_INTERVAL)
         twm.join()
